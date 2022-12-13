43e986ef52689c5a5d0f49da254217c024bbf089
nasirisreal <nasirisreal@yahoo.com>
2022-12-13 07:40:40 -0800

    Alphat

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
